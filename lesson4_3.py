ae5723cd523646ebda8ba0ef9b61b0e4b1c61ad8
hinamuradev <hinamuradev@mail.ru>
2023-11-09 16:35:24 +0300

    lesson 4 dz

--- lesson4_3.py
+++ lesson4_3.py
@@ -2,6 +2,5 @@
 # а значениями вложенный словарь с ключами "name" и "email",
 # а значения для этих ключей будут браться с клавиатуры
 n = int(input("Введите число: "))
-name = input()
-my_dict = {n: {name: email for email in input("Введите выходное")} for n in range(1, n+1)}
+my_dict = {n: [{"Name": values1 for values1 in input("Введите Имя")},{"Email": values2 for values2 in input("Введите мыло")}] for n in range(1, n+1)}
 print(my_dict)
