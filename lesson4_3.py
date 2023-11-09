8e979887a47f5d1c1d1c0b37d586e33160eebd83
hinamuradev <hinamuradev@mail.ru>
2023-11-09 16:28:38 +0300

    lesson 4 dz

--- lesson4_3.py
+++ lesson4_3.py
@@ -1,8 +1,7 @@
 # Заполнить словарь где ключами будут выступать числа от 0 до n,
 # а значениями вложенный словарь с ключами "name" и "email",
 # а значения для этих ключей будут браться с клавиатуры
-n = int(input())
-name = input("Введите имя: ")
-email = input("Введите мыло: ")
-my_dict = { n:{name:email} for n in range(1,n)}
+n = int(input("Введите число: "))
+name = input()
+my_dict = {n: {name: email for email in input("Введите выходное")} for n in range(1, n+1)}
 print(my_dict)
