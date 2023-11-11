b449e8ab9ba0d3e17ee55d0645abf5f621a732cb
hinamuradev <hinamuradev@mail.ru>
2023-11-11 13:59:54 +0300

    lesson 4 dz full

--- lesson4_3.py
+++ lesson4_3.py
@@ -2,10 +2,5 @@
 # а значениями вложенный словарь с ключами "name" и "email",
 # а значения для этих ключей будут браться с клавиатуры
 n = int(input("Введите число: "))
-my_dict = {}
-for num in range(1, n + 1):
-    name = input("name")
-    email = input("email")
-    nested_dict = {"name": name, "email": email}
-    my_dict[num] = nested_dict
+my_dict = {n :{"name": input("Name"), "email": input("Email") } for n in range(1, n+1)}
 print(my_dict)
