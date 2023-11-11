dedf4bb63f0f29fea75dbeb01ed7b50a4af2969b
hinamuradev <hinamuradev@mail.ru>
2023-11-11 15:00:58 +0300

    lesson 4 dz full

--- lesson4_2.py
+++ lesson4_2.py
@@ -1,9 +1,5 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
 text = input()
-# objs = dict.fromkeys(text, 0)
-# for i in text:
-#     objs[i] += 1
-# print(objs)
-objs = {i : text for i in text}
+objs = {s: text.count(s) for s in list(text)}
 print(objs)
