2d76e690cde55a7720e6541b0e060ef2e0a94422
hinamuradev <hinamuradev@mail.ru>
2023-11-11 11:30:28 +0300

    lesson 4 dz not full

--- lesson4_2.py
+++ lesson4_2.py
@@ -1,5 +1,9 @@
 # Без использования collections, написать программу, которая будет создавать словарь
 # для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
-my_dict = {}
-text = input()
+# text = input("Введите текст")
+# objs = {i: text[i] for i in range(len(text.count()))}
+# print(objs)
 
+# text = "HELLO"
+# data = dict(enumerate(text))
+# # print(data)
