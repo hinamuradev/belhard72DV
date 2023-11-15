498b1b8738aed169e1087a3a4978e94bb65167d2
hinamuradev <hinamuradev@mail.ru>
2023-11-15 10:02:10 +0300

    new lesson5

--- lesson5_2.py
+++ lesson5_2.py
@@ -1,3 +1,24 @@
 # Сделать калькулятор: у пользователя
 # спрашивается число, потом действие и второе
 # число
+
+while True:
+    s = input()
+    if s in ('+', '-', '*', '/'):
+        a = float(input("a ="))
+        b = float(input("b ="))
+
+    match s:
+        case '+':
+            print(a+b)
+        case '-':
+            print(a - b)
+        case '*':
+            print(a * b)
+        case '/':
+            if b != 0:
+                print(a / b)
+            else:
+                print("Деление на нуль!")
+        case 0:
+            break
