0d803209838e1f556f5aaa52e0d9652765e382e8
hinamuradev <hinamuradev@mail.ru>
2023-11-15 09:51:48 +0300

    new lesson5

--- lesson5_1.py
+++ lesson5_1.py
@@ -1,1 +1,7 @@
 # Вывести первые N цисел кратные M и больше K
+n = int(input())
+m = int(input())
+k = int(input())
+for n in range(1, n + 1):
+    if n % m == 0 and n > k:
+        print(f"Числа {n} кратные числу {m} и больше {k}")
