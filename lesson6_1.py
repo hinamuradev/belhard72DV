e2e237dbed52ca93695e4dfd37f9d39d23ee3750
hinamuradev <hinamuradev@mail.ru>
2023-11-17 09:48:19 +0300

    lesson 6 dz

--- lesson6_1.py
+++ lesson6_1.py
@@ -1,10 +1,23 @@
-#Написать функцию перевода десятичного числа в двоичное и обратно, без
-# использования функции int
+#Написать функцию перевода десятичного числа в двоичное и обратно, без использования функции int
 
-def is_number(a):
-    n = ""
-    while a > 0:
-        n = str(a % 2) + n
-        a //= 2
-    return n
-
+# def is_number(a):
+#     a = 45
+#     n = ""
+#     s = ""
+#     while a > 0:
+#         n = str(a % 2) + n
+#         a //= 2
+#     return n
+#
+# print(is_number(a=45))
+# def convert_to(number, base, upper=False):
+#     digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+#     if base > len(digits):
+#         return None
+#     result = ''
+#     while number > 0:
+#         result = digits[number % base] + result
+#         number //= base
+#     return result.upper() if upper else result
+#
+# print(convert_to(100))
