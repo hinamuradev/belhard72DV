c4a7e75c2478563eb008b084b39f68459fd7cebc
hinamuradev <hinamuradev@mail.ru>
2023-11-18 16:40:57 +0300

    lesson 6 dz

--- lesson6_4.py
+++ lesson6_4.py
@@ -2,3 +2,14 @@
 # образом, чтобы остались только строки, использование дополнительного списка
 # незаконно
 
+lists = ["gfdgdf", 12 , 34, "fdsfsd"]
+
+def is_onn(word):
+    words = []
+    for n in lists:
+        if type(n) == str:
+            words.append(n)
+    print(words)
+
+is_onn(1)
+
