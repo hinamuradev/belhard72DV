08b89a31d3a77ffc8c4ed71b0c92f24da3e1f601
hinamuradev <hinamuradev@mail.ru>
2023-12-03 23:03:17 +0300

    lesson 6 dz

--- lesson_test.py
+++ lesson_test.py
@@ -1,4 +1,38 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-result = list(filter(lambda elem: elem in b, a))
-print(result)
+import yaml
+
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+def get_tasks_for_build(build_name, builds_data, tasks_data):
+    build_tasks = builds_data.get(build_name, [])
+    all_tasks = []
+
+    for task_name in build_tasks:
+        task_info = tasks_data.get(task_name, {})
+        task_list = [task_name]
+
+        subtasks = task_info.get('subtasks', [])
+        while subtasks:
+            subtask_name = subtasks.pop(0)
+            task_list.append(subtask_name)
+            subtasks.extend(tasks_data[subtask_name].get('subtasks', []))
+
+        all_tasks.extend(task_list)
+
+    return all_tasks
+
+# Загрузка данных из YAML файлов
+builds_data = load_yaml('builds.yaml')
+tasks_data = load_yaml('tasks.yaml')
+
+# Пример использования для билда "forward_interest"
+build_name = 'forward_interest'
+build_tasks = get_tasks_for_build(build_name, builds_data, tasks_data)
+
+# Вывод списка задач для выполнения билда
+print(f"Билд - {build_name}")
+print("Список задач:")
+for task in build_tasks:
+    print(task)
